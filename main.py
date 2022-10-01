f9f194c1b69820dbbdcc9fd40a983635e4579203
makuntz <maira.kuntz@animati.com.br>
2022-10-01 09:49:58 -0300

    teste2

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+# import logging
+# import uvicorn
+# from api.core import settings
+# from api.app import app
+
+
+# logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host=settings.HOST, port=settings.PORT, reload=True)
+
 import logging
 import uvicorn
+from fastapi import FastAPI
+from api.routers import api_router
 from api.core import settings
-from api.app import app
-
+from api.server.database import connect_db, close_conn_db
 
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+
+app = FastAPI(title=settings.NAME_APP)
+
+# add conection database
+app.add_event_handler('startup', connect_db)
+app.add_event_handler('shutdown', close_conn_db)
+
+# add routes
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host=settings.HOST, port=settings.PORT, reload=True)
